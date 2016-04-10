39d5374b0af7e9ef1c3bdc74a27458d37b6cd99a
Isaiah J Turner <github.com@isaiahjturner.com>
2016-04-10 09:19:21 -0400

    cool

--- app.js
+++ app.js
@@ -23,6 +23,30 @@ app.get("/connect", function(req, res) {
         savedRes = res;
         return;
     }
+    needle.post('https://api.contactually.com/v2/contacts', '{\
+  "data": {\
+    "first_name": "' + contactInfo.name + '",\
+    "last_name": "",\
+    "company": null,\
+    "title": "' + contactInfo.name + '",\
+    "avatar_url": null,\
+    "email_addresses": [\
+      {\
+        "label": null,\
+        "address": "' + contactInfo.email + '"'
+        '}\
+    ]\
+  }\
+}', {
+            json: true,
+            headers: {
+              Authorized: "Bearer 5c678fa1c39749d9859a93e651ff959f33e0ecf494a12cdc27e127f51e653bb9"
+            }
+
+        },
+        function(err, resp) {
+            console.log(err, resp)
+        })
     sp.transmissions.send({
         transmissionBody: {
             content: {
@@ -68,27 +92,27 @@ app.get("/connect", function(req, res) {
     savedRes = null;
 });
 app.get("/connect2", function(req, res) {
-  contactInfo = false;
-  savedRes = false;
-  sp.transmissions.send({
-      transmissionBody: {
-          content: {
-              from: 'testing@sparkpostbox.com',
-              subject: 'You connected with ' + req.query.name,
-              html: '<html><body><p>Hi ' + req.query.name + ',<br/> You are now connected with Hillary Sanders. She will reach out to you shortly.<br><br>Thanks, <br>Contap</p></body></html>'
-          },
-          recipients: [{
-              address: req.query.email
-          }]
-      }
-  }, function(err, res) {
-      if (err) {
-          console.log('Whoops! Something went wrong');
-          console.log(err);
-      } else {
-          console.log('Woohoo! You just sent your first mailing!');
-      }
-  });
+    contactInfo = false;
+    savedRes = false;
+    sp.transmissions.send({
+        transmissionBody: {
+            content: {
+                from: 'testing@sparkpostbox.com',
+                subject: 'You connected with ' + req.query.name,
+                html: '<html><body><p>Hi ' + req.query.name + ',<br/> You are now connected with Hillary Sanders. She will reach out to you shortly.<br><br>Thanks, <br>Contap</p></body></html>'
+            },
+            recipients: [{
+                address: req.query.email
+            }]
+        }
+    }, function(err, res) {
+        if (err) {
+            console.log('Whoops! Something went wrong');
+            console.log(err);
+        } else {
+            console.log('Woohoo! You just sent your first mailing!');
+        }
+    });
 });
 app.get("/contacts", function(req, res) {
     res.json({
